e359edc1e4a79e15bf821e2fdc8640259e754477
Dravidkumar180 <167848429+Dravidkumar180@users.noreply.github.com>
2026-02-02 16:36:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+// const a= 24
+// const a=23
+// console.log(a)
+
+// let x = 10;
+// let x = 20;
+// var user1 = 20;
+// var user1 = 30;
+// console.log(user1);
+var xname = 5;
+letxname = 40;
+console.log(xname);
+
+var name = "Alex";
+var age = 25;
+
+const name1 = "Alex";
+     age = 25;
+
+console.log(name1); 
+console.log(age)
+
+var name = "raja";
+name="kerli";
+var name = "suuman";
+console.log("name:-", name);
+
+let city = "AP";
+    city="KA";
+console.log("city:-", city);
+
+const company= "LLC";
+    //   company ="stackly";
+console.log("company:-", company);
