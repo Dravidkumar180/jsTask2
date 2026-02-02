db1010c676aa31f90b9b7f318bf2798d81856b6a
Dravidkumar180 <167848429+Dravidkumar180@users.noreply.github.com>
2026-02-02 20:30:13 +0530

    Add files via upload

---
+++ js_task2.js
@@ -0,0 +1,196 @@
+// 1. Welcome Program
+// - Ask user name using prompt().
+// - Show alert: Hi , Welcome to JavaScript Training.
+// - Print the same message in console.
+
+// let username = prompt("Dravidkumar");
+// let username1= alert("Hi , Welcome to JavaScript Training.")
+// console.log("Hi , Welcome to JavaScript Training.");
+
+// 2. Console Methods Practice
+// - Print 500 using console.log(), console.warn(), console.error().
+// - Use console.clear().
+
+// console.log(500);
+// console.warn(500);
+// console.error(500);
+// console.clear();
+
+// 3. Data Type Identification
+// - Create string, number, boolean, undefined, and null variables.
+// - Print value and type using typeof.
+
+// let a = ("50")
+// console.log(typeof(a));
+
+// let b=(76)
+// console.log(typeof(b));
+
+// let c=(true)
+// console.log(typeof(c));
+
+// let d 
+// console.log(typeof(d));
+
+// let e = prompt()
+// console.log(e);
+
+// 4. Arithmetic Operations
+// - Use let a = 20 and let b = 5.
+// - Perform +, -, *, /, %, ** and print results.
+
+// let a1=20
+// let b1=5
+// console.log(a1+b1);
+
+// let a2=20
+// let b2=5
+// console.log(a2-b2);
+
+// let a3=20
+// let b3=5
+// console.log(a3*b3);
+
+// let a4=20
+// let b4=5
+// console.log(a4/b4);
+
+// let a5=20
+// let b5=5
+// console.log(a5%b5);
+
+// let a6=20
+// let b6=5
+// console.log(a6**b6);
+
+// 5. Increment & Decrement
+// - Demonstrate pre-increment, post-increment, pre-decrement, post-decrement.
+// - Print variables clearly.
+
+// post increment
+// let num = 10;
+//     num = num++;
+// console.log(num);
+
+// pre increment
+// let num1 =10;
+// let num2= ++num1;
+// let num3= ++num2;
+// console.log(num1,num2,num3);
+
+// pre decrement
+// let num4=52;
+// num4= --num4;
+// console.log(num4);
+
+// post decrement
+// let num5=32;
+// let num6= num5--;
+// console.log(num5,num6);
+
+// 6. Assignment Operators
+// - Use let num = 10.
+// - Perform +=, -=, *=, /=, %= and print results.
+
+// let num8 = 10;
+// num8 += 10;
+// console.log(num8);
+
+// let num9 = 10;
+// num9 -= 10;
+// console.log(num9);
+
+// let num10 = 10;
+// num10 *= 10;
+// console.log(num10);
+
+// let num11 = 10;
+// num11 /= 10;
+// console.log(num11);
+
+// let num12 = 10;
+// num12 %= 10;
+// console.log(num12);
+
+
+// 7. Array Access
+// - Create array ['HTML','CSS','JavaScript','React'].
+// - Print first element, second element, last element (using length), and total elements.
+
+// let lan = ['HTML','CSS','JavaScript','React']
+// console.log(lan[0]);
+// console.log(lan[1]);
+// console.log(lan[lan.length-1]);
+// console.log(lan.length);
+
+// 8. Modify Array
+// - Add one element at end.
+// - Remove last element.
+// - Print updated array.
+
+// let arr = ['apple','orange','mango','papaya','kiwi']
+// console.log(arr);
+// console.log(arr[arr.length-1]);
+
+// 9. Student Object
+// - Create object with name, age, course, city.
+// - Print values using dot notation.
+
+// let ob = {
+//     name: ['ram','kumar','prabu'],
+//     age: ["25","30","22"],
+//     course:["csc","ece","eee"],
+//     city: ["ka","tn","kl"]
+// }
+//  console.log(ob.name);
+//  console.log(ob.age);
+//  console.log(ob.course);
+//  console.log(ob.city);
+ 
+ 
+
+
+// 10. Nested Object Practice
+// - Create company object with nested trainer object.
+// - Print company name, trainer name, trainer subject.
+
+// let nested_trainer_ob ={
+//     company_name:['llc','infy','cts'],
+//     trainer_name:['sam','kesv','pranav'],
+//     trainer_sub:['js','django','mongo db']
+// }
+// console.log(nested_trainer_ob.company_name[2]);
+// console.log(nested_trainer_ob.trainer_name[0]);
+// console.log(nested_trainer_ob.trainer_sub[2]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
